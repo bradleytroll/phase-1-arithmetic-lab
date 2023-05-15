20eeba038d9edbd0849a564dda2c6a9bfb669ea8
Bradley Troll <bradleytroll@gmail.com>
2023-05-15 13:45:02 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
+const random = Math.floor(Math.random() + 1);
+
+let num3 = 34
+let num4 = 30
+let mod = num3 - num4
+
+const max = 
+Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
