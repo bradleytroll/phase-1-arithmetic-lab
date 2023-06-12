b96b99c547e75f7a4045fa05061c412d016e13bc
Bradley Troll <bradleytroll@gmail.com>
2023-06-12 16:14:18 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-let num1 = 31
-let num2 = 2
-let multiply = num1 * num2
+// Redid the lab for extra practice. New code below; original code commented out beneath.
 
-const random = Math.floor(Math.random() + 1);
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-let num3 = 34
-let num4 = 30
-let mod = num3 - num4
+let random = Math.floor(Math.random() * 10) + 1;
 
-const max = 
-Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+let num3 = 12
+let num4 = 3
+let mod = num3 / num4;
+
+let numbers = (2, 5, 18, 20);
+let max = Math.max(numbers);
+
+// let num1 = 31
+// let num2 = 2
+// let multiply = num1 * num2
+
+// const random = Math.floor(Math.random() + 1);
+
+// let num3 = 34
+// let num4 = 30
+// let mod = num3 - num4
+
+// const max = 
+// Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
